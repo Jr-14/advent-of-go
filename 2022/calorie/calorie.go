d48d36f30d7021a4c20f611ca5692f19af20d746
John Requizo <jereq1@student.monash.edu>
2023-02-25 22:21:05 +1100

    Learn how to read from file using scanner

--- 2022/calorie/calorie.go
+++ 2022/calorie/calorie.go
@@ -1,1 +1,36 @@
 package calorie
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func BufferReadPrintCalorier(fileName string) {
+	// Get file size
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileInformation, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The file is %d bytes long", fileInformation.Size())
+
+	scanner := bufio.NewScanner(file)
+	newLineCounter := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			newLineCounter += 1
+			fmt.Printf("Hello I am the %d newline\n", newLineCounter)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
--- 2022/main.go
+++ 2022/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Jr-14/advent-of-go/2022/calorie"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	calorie.BufferReadPrintCalorier("calorie/1-input.txt")
 }
