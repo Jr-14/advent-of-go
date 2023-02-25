74fe33e90a4d8be441df04a12496577d104b9878
John Requizo <jereq1@student.monash.edu>
2023-02-26 00:33:14 +1100

    Compelte Part two of Advent of Code Day 2 2022

--- 2022/rps/rps.go
+++ 2022/rps/rps.go
@@ -43,6 +43,69 @@ func mapChoice(rpsChoice string) RockPaperScissors {
 	}
 }
 
+func determineMyChoice(opponentChoice RockPaperScissors, winningCondition string) RockPaperScissors {
+	rock := RockPaperScissors{"Rock", 1}
+	paper := RockPaperScissors{"Paper", 2}
+	scissors := RockPaperScissors{"Scissors", 3}
+	switch winningCondition {
+	case "lose":
+		switch opponentChoice {
+		case rock:
+			return scissors
+		case paper:
+			return rock
+		case scissors:
+			return paper
+		default:
+			panic("oops")
+		}
+	case "win":
+		switch opponentChoice {
+		case rock:
+			return paper
+		case paper:
+			return scissors
+		case scissors:
+			return rock
+		default:
+			panic("oops")
+		}
+	case "draw":
+		return opponentChoice
+	default:
+		panic("oops")
+	}
+}
+
+func determineStrategy(encryptedStrategy string) string {
+	switch encryptedStrategy {
+	case "X":
+		return "lose"
+	case "Y":
+		return "draw"
+	case "Z":
+		return "win"
+	default:
+		panic("I can't win")
+	}
+}
+
+func calculateRoundScorePartTwo(opponentChoice string, myChoice string) int {
+	opponentPlay := mapChoice(opponentChoice)
+	strategy := determineStrategy(myChoice)
+	toPlay := determineMyChoice(opponentPlay, strategy)
+	switch strategy {
+	case "lose":
+		return 0 + toPlay.points
+	case "draw":
+		return 3 + toPlay.points
+	case "win":
+		return 6 + toPlay.points
+	default:
+		panic("no points")
+	}
+}
+
 func calculateRoundScore(opponentChoice string, myChoice string) int {
 	opponentPlay := mapChoice(opponentChoice)
 	myPlay := mapChoice(myChoice)
@@ -72,7 +135,7 @@ func PlayRockPaperScissors(fileName string) int {
 	for scanner.Scan() {
 		text := scanner.Text()
 		opponentChoice, myChoice := parse(text)
-		totalScore += calculateRoundScore(opponentChoice, myChoice)
+		totalScore += calculateRoundScorePartTwo(opponentChoice, myChoice)
 	}
 	return totalScore
 }
