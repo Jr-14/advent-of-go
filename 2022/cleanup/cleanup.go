96bab4001b8c5622992a025717ffadb137d70c8c
John Requizo <jr.jemerson14@gmail.com>
2023-02-27 00:22:28 +1100

    Complete Part One of Advent of Code Day 4 2022

--- 2022/cleanup/cleanup.go
+++ 2022/cleanup/cleanup.go
@@ -1,1 +1,67 @@
 package cleanup
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Section struct {
+	low  int
+	high int
+}
+
+func isFullyOverlapping(firstSection Section, secondSection Section) bool {
+	if firstSection.low >= secondSection.low && firstSection.high <= secondSection.high {
+		return true
+	}
+	if secondSection.low >= firstSection.low && secondSection.high <= firstSection.high {
+		return true
+	}
+	return false
+}
+
+func parseSection(section string) Section {
+	substring := strings.Split(section, "-")
+	low, err := strconv.Atoi(substring[0])
+	if err != nil {
+		panic(err)
+	}
+	high, err := strconv.Atoi(substring[1])
+	if err != nil {
+		panic(err)
+	}
+	return Section{low, high}
+}
+
+func parseAssignmentPairs(assignments string) (string, string) {
+	substring := strings.Split(assignments, ",")
+	return substring[0], substring[1]
+}
+
+func retrieveAssignmentPairs(assignments string) (Section, Section) {
+	assignmentOne, assignmentTwo := parseAssignmentPairs(assignments)
+	elfSectionOne := parseSection(assignmentOne)
+	elfSectionTwo := parseSection(assignmentTwo)
+	return elfSectionOne, elfSectionTwo
+}
+
+func CountFullOverlappingAssignments(fileName string) int {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	sum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		elfAssignmentOne, elfAssignmentTwo := retrieveAssignmentPairs(text)
+		if isFullyOverlapping(elfAssignmentOne, elfAssignmentTwo) {
+			sum += 1
+		}
+	}
+	return sum
+}
--- 2022/main.go
+++ 2022/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	// "github.com/Jr-14/advent-of-go/2022/rps"
 	"fmt"
 
-	"github.com/Jr-14/advent-of-go/2022/rucksack"
+	"github.com/Jr-14/advent-of-go/2022/cleanup"
 )
 
 func main() {
-	// score := rps.PlayRockPaperScissors("rps/input.txt")
-	// fmt.Printf("My Max Score is %d.", score)
-	priorities := rucksack.CalculateRuckSackPartTwo("rucksack/input.txt")
-	fmt.Printf("Sum of priorities in Rucksack is %d.", priorities)
+	overlapping := cleanup.CountFullOverlappingAssignments("cleanup/input.txt")
+	fmt.Printf("Sum of priorities in Rucksack is %d.", overlapping)
 }
