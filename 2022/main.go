ee64b8ea6529b86e35be569c958ca9123ce92272
John Requizo <jereq1@student.monash.edu>
2023-02-26 22:25:27 +1100

    completed Part one of advent of code day 3 2022

--- 2022/main.go
+++ 2022/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	// "github.com/Jr-14/advent-of-go/2022/rps"
 	"fmt"
 
-	"github.com/Jr-14/advent-of-go/2022/rps"
+	"github.com/Jr-14/advent-of-go/2022/rucksack"
 )
 
 func main() {
-	score := rps.PlayRockPaperScissors("rps/input.txt")
-	fmt.Printf("My Max Score is %d.", score)
+	// score := rps.PlayRockPaperScissors("rps/input.txt")
+	// fmt.Printf("My Max Score is %d.", score)
+	priorities := rucksack.CalculateRuckSack("rucksack/input.txt")
+	fmt.Printf("Sum of priorities in Rucksack is %d.", priorities)
 }
--- 2022/rucksack/rucksack.go
+++ 2022/rucksack/rucksack.go
@@ -1,1 +1,57 @@
 package rucksack
+
+import (
+	"bufio"
+	"os"
+)
+
+func calculateCharacterPriority(character string) int {
+	asciiRep := int(character[0])
+	// lowercase characters
+	var priority int
+	if asciiRep >= 97 && asciiRep <= 122 {
+		priority = asciiRep - 96
+	} else {
+		priority = asciiRep - 38
+	}
+	return priority
+}
+
+func splitToCompartments(items string) (string, string) {
+	return items[:len(items)/2], items[len(items)/2:]
+}
+
+func calculateRucksackPriority(rucksack string) int {
+	firstCompartment, secondCompartment := splitToCompartments(rucksack)
+	firstCompartmentArray := [53]int{0}
+	var priority int
+	for i := 0; i < len(firstCompartment); i++ {
+		fcCharacter := string(firstCompartment[i])
+		fcPriority := calculateCharacterPriority(fcCharacter)
+		firstCompartmentArray[fcPriority]++
+	}
+	for i := 0; i < len(secondCompartment); i++ {
+		scCharacter := string(secondCompartment[i])
+		scPriority := calculateCharacterPriority(scCharacter)
+		if firstCompartmentArray[scPriority] > 0 {
+			priority = scPriority
+			break
+		}
+	}
+	return priority
+}
+
+func CalculateRuckSack(fileName string) (sum int) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		rucksack := scanner.Text()
+		sum += calculateRucksackPriority(rucksack)
+	}
+	return
+}
